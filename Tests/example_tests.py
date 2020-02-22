3dda72debbdfff7be2ecab6a038df69fa087db66
anmorales-hub <moralesand2018@gmail.com>
2020-02-21 23:07:10 -0500

    Fix: fix tests

--- Tests/example_tests.py
+++ Tests/example_tests.py
@@ -5,14 +5,9 @@ from examples.abstraction import SquareRoot
 class MyTestCase(unittest.TestCase):
     def test_Examples_root(self):
         self.assertEqual(6, SquareRoot.squareRoot(36))
-        self.assertEqual(6, SquareRoot,squareRootA(36))
-
-
-
-
+        self.assertEqual(6, SquareRoot.squareRootA(36))
 
 
 
 if __name__ == '__main__':
-
     unittest.main()
